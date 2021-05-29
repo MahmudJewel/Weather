ca12885baa8fb8ad085f6d1d48a14ef2c8f797fb
villain <villain@kali.local>
2021-05-30 01:56:50 +0600

    new change for heroku

--- weather/urls.py
+++ weather/urls.py
@@ -16,13 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
 	path('',include('weather_sub.urls')),
     path('admin/', admin.site.urls),
-
-    
-
 ]
