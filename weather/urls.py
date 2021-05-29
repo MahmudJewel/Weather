3952a1078319c76a9e2cf48748633c07928ea808
Jewel Mahmud <35461355+MahmudJewel@users.noreply.github.com>
2021-05-29 20:58:52 +0600

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.views.static import serve
-form django.conf.urls import url
+from django.conf.urls import url
 
 urlpatterns = [
 	path('',include('weather_sub.urls')),
